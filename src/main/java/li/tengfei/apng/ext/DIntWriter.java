dcd21c22b75038f5d00b541b4ad10e5cc3c46103
Li Tengfei <i@tengfei.li>
2016-12-10 11:59:07 +0800

    add Dynamic Integer data type  and support classes and tests

---
+++ src/main/java/li/tengfei/apng/ext/DIntWriter.java
@@ -0,0 +1,66 @@
+package li.tengfei.apng.ext;
+
+import li.tengfei.apng.base.patch.DInt;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Writer for DInt
+ *
+ * @author ltf
+ * @since 16/12/9, 下午3:32
+ */
+public class DIntWriter extends DInt {
+
+    public static final int HAVE_NEXT_BYTE_MASK = -128;
+    private byte[] data = new byte[4];
+
+    public DIntWriter() {
+    }
+
+    public DIntWriter(int value) {
+        setValue(value);
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+        size = 0;
+        byte v = (byte) (value & VALUE_MASK);
+        value >>= 7;
+        data[size++] = value == 0 ? v : (byte) (v | HAVE_NEXT_BYTE_MASK);
+        while (value != 0) {
+            v = (byte) (value & VALUE_MASK);
+            value >>= 7;
+            data[size++] = value == 0 ? v : (byte) (v | HAVE_NEXT_BYTE_MASK);
+
+            if (size == 3 && value != 0) {
+                data[size++] = (byte) (value & 0xff);
+                if (value >> 8 != 0) throw new IllegalStateException("DInt value over range");
+                break;
+            }
+        }
+    }
+
+    /**
+     * write DInt to array
+     *
+     * @param dst    dest array
+     * @param offset dest array offset
+     * @return writed size
+     */
+    public int write(byte[] dst, int offset) {
+        System.arraycopy(data, 0, dst, offset, size);
+        return size;
+    }
+
+    /**
+     * write DInt to OutputStream
+     *
+     * @return writed size
+     */
+    public int write(OutputStream os) throws IOException {
+        os.write(data, 0, size);
+        return size;
+    }
+}
---
+++ src/test/java/li/tengfei/apng/ext/DIntWriterTest.groovy
@@ -0,0 +1,37 @@
+package li.tengfei.apng.ext
+
+import li.tengfei.apng.base.patch.DInt
+import org.testng.annotations.Test
+
+import static org.testng.Assert.assertEquals
+
+/**
+ *
+ * @author ltf
+ * @since 16/12/9, 下午4:24
+ */
+class DIntWriterTest {
+
+    @Test
+    void testDInt() {
+        byte[] cache = new byte[10]
+        byte[] readedSize = new byte[1]
+        int size = -1
+        DIntWriter writer = new DIntWriter()
+        DInt dInt = new DInt()
+        for (int i = DInt.MIN_DINT_VALUE; i <= DInt.MAX_DINT_VALUE; i++) {
+            dInt.reset()
+            writer.setValue(i)
+
+            if (size != writer.size) {
+                size = writer.size
+                println(String.format("size: %d  i: %d  v: %d ", size, writer.value, i))
+            }
+
+            assertEquals writer.write(cache, 3), size
+            assertEquals dInt.readValue(cache, 3, readedSize), i
+            assertEquals readedSize[0], size
+            assertEquals writer.value, dInt.value
+        }
+    }
+}
